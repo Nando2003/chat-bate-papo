cd01e560fec538839c7f0f179cb10b3e2edb9f14
Nando2003 <nandofontes27@gmail.com>
2025-09-17 16:18:28 -0300

    adding loadmore

--- start/socket.ts
+++ start/socket.ts
@@ -19,7 +19,7 @@ app.ready(async () => {
   io.on('connection', (socket) => {
     const userId = socket.data.userId as number
     if (!userId) return
-  
+
     const chatService = new ChatService(userId)
 
     socket.on('joinRoom', async (room: string) => {
